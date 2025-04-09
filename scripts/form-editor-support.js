ebde7c054f08072addc55fccccd03f0fb8011907
Venkatesh Sampath <venkatesh.sampathk@gmail.com>
2025-04-09 09:33:38 +0530

    add

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -359,7 +359,7 @@ function enableAuthoringAssistantExtension() {
   meta.name = 'urn:adobe:aue:config:extensions';
 
   //meta.content = 'https://localhost.corp.adobe.com:8013/resources/universal_editor.html?livecycle-forms-spa_version=local'; 
-meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-545-9aa1df979f7b9971b6d022da2663969c331d8725';
+meta.content =   'https://experience.adobe.com/solutions/livecycle-forms-spa/static-assets/resources/universal_editor.html?livecycle-forms-spa_version=PR-545-2dc9f7374e13b8c42f4b3daa651e3d3e62d0a96d';
   //PR-530-51438302f0972ee340e528ce2ff3fac4b1bef201
 
  console.log('Adding meta tag for aem forms authoring assistant extension:', meta.content);
